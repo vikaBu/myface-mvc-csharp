fceda3660a81830c7869a22d7a65657a7e72675f
Mike <Walker>
2020-02-19 13:38:31 +0000

    Added API endpoint for information about a user.

---
+++ MyFace/Controllers/UsersApiController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MyFace.Models.api;
+using MyFace.Models.Database;
+using MyFace.Models.View;
+using MyFace.Repositories;
+
+namespace MyFace.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersApiController : ControllerBase
+    {
+        private IUsersRepo _users;
+
+        public UsersApiController(IUsersRepo users)
+        {
+            _users = users;
+        }
+
+        [Route("{id}")]
+        public ActionResult<UserApiModel> GetUser(int id)
+        {
+            var user = _users.GetById(id);
+            return new UserApiModel(user);
+        }
+    }
+}
---
+++ MyFace/Models/api/UserApiModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using MyFace.Models.Database;
+
+namespace MyFace.Models.api
+{
+    public class UserApiModel
+    {
+        private User _user;
+
+        public UserApiModel(User user)
+        {
+            _user = user;
+        }
+
+        public int Id => _user.Id;
+        public string FirstName => _user.FirstName;
+        public string LastName => _user.LastName;
+        public string DisplayName => $"{FirstName} {LastName}";
+        public string Username => _user.Username;
+        public string Email => _user.Email;
+        public string ProfileImageUrl => _user.ProfileImageUrl;
+        public string CoverImageUrl => _user.CoverImageUrl;
+        public int NumberOfPosts => _user.Posts.Count;
+        public DateTime? DateOfLastPost => _user.Posts.FirstOrDefault()?.PostedAt;
+    }
+}
